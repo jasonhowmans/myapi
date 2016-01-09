089b2df479dda1b8d8c9daa868568e0f33b18b79
Jason Howmans <jasonhowmans@gmail.com>
2016-01-09 00:32:35 +0000

    Add more meat to the rss feed endpoint.

--- api/routes/rss.js
+++ api/routes/rss.js
@@ -2,7 +2,6 @@
 var storage = require('node-persist');
 var RSS = require('rss');
 
-
 module.exports = function feedHandler (req, res, next) {
   var feedOptions = {
     title: 'nosaj.io',
@@ -11,7 +10,33 @@ module.exports = function feedHandler (req, res, next) {
     site_url: 'http://nosaj.io',
     ttl: 15
   };
+
+  var storageOptions = {
+    dir: '../../persist',
+    ttl: false
+  };
+
   var feed = new RSS( feedOptions );
+  storage.initSync( storageOptions );
+
+  var posts = storage.values();
+
+  if (! posts) {
+    console.error('Posts returned empty for rss endpoint');
+  }
+
+  posts.forEach( function (post) {
+    var feedItem = {
+      title: post.title,
+      description: post.synopsis,
+      link: `http://nosaj.io/#/${post.meta.slug}`,
+      author: 'Jason Howmans',
+      date: new Date(post.meta.date)
+    };
+
+    feed.item( feedItem )
+  });
+
 
   res.setHeader('Content-Type', 'application/rss+xml');
   res.end( feed.xml() );
